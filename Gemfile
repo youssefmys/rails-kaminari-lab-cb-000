e7320914bfdf5c548d068ce9fce8984f869934fc
youssefmys <mosa500@gmail.com>
2018-04-07 23:23:21 +0200

    Done

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'kaminari'
 gem 'rails', '4.2.5'
 gem 'sqlite3'
 gem 'sass-rails', '~> 5.0'
@@ -17,4 +18,3 @@ end
 group :development do
   gem 'web-console', '~> 2.0'
 end
-
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,6 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    @songs = Song.order(:created_at => :desc).page(params[:page])
   end
 
   def show
@@ -50,4 +50,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
